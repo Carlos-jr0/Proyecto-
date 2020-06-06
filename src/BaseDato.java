bb0445e8eec5e05cfe63ff0b215798986f90eb2a
Your Name <alvaradocharly32@gamil.com>
2020-06-06 16:46:09 -0600

    Nueva Actualizacion

--- src/BaseDato.java
+++ src/BaseDato.java
@@ -11,10 +11,22 @@ public class BaseDato {
 	private Stack<Entidad> entidades = new Stack<Entidad>();
     private String path;
     private boolean DEBUG = true; // Seteamos un debug mode, si está en TRUE mostramos todos los mensajes de debug
+    private Entidad entidadActual; // Creamos una variable entidad para crear registros. Si el DB tiene una entidad se convierte en manager de esa entidad
+    private Stack<String> archivos = new Stack<String>();
     
     /* Inicializamos la BD con una ruta para guardar las entidades */
     BaseDato(String path) {
         this.path = path;
+    }
+    
+    // Devuelve la entidad actual de la BD, si la BD es un manager.
+    public Entidad getToPersist() {
+        return entidadActual;
+    }
+    
+ // Seteamos una entidad actual para convertir el BD en un manager de esa entidad.
+    public void setToPersist(Entidad en) {
+        this.entidadActual = en;
     }
     
     /* Agregamos una entidad */
@@ -28,10 +40,10 @@ public class BaseDato {
     }
     
     /* Mostramos las entidades que existen en la BD */
-    public void mostrarEntidad() {
+    public void mostrarEntidades() {
         Stack<Entidad> mostrarEntidades = entidades;
         
-        while(!mostrarEntidades.empty()) {
+        while(!mostrarEntidades.empty()) {//si mostrar entidades esta vacia
             Entidad en = mostrarEntidades.pop();
             
             if (DEBUG)
@@ -39,20 +51,21 @@ public class BaseDato {
         }
     }
     
-    /* Verificamos si existen archivos de entidad, si existen agregamos las entidades a la DB */
+    /* Verificamos si existen archivos de entidad, si existen agregamos las entidades a la BD */
     public void checkEntidades() throws FileNotFoundException, IOException {
-        File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
-
-        for (File file : listOfFiles) {
+        File carpeta = new File(path);
+        File[] listaDeArchivos = carpeta.listFiles();
+        
+        if (listaDeArchivos.length > 0) {
+        for (File file : listaDeArchivos) {
             if (file.isFile()) {
                 BufferedReader br = new BufferedReader(new FileReader(path + file.getName()));
                 Entidad en = new Entidad();
-                String line = br.readLine();
-                String[] splitted = line.split(",");//linea dividida por ","
-                String[] atrs = splitted[0].split(".");
+                String linea = br.readLine();
+                String[] separado = linea.split(",");//linea dividida por ","
+                String[] atrs = separado[0].split(".");
                 
-                en.setNombre(splitted[0]);
+                en.setNombre(separado[0]);
                 
                 for (String attr : atrs) {
                     String[] atr = attr.split("-");
@@ -63,22 +76,98 @@ public class BaseDato {
             }
         }
     }
+  }      
     
-    /* Creamos los archivos de entidad, un archivo .txt por cada entidad */
-    public void archivoEntidad() throws IOException {
-        Stack<Entidad> crearEntidades = entidades;
+    /* Revisamos si existen archivos de la entidad actual y los agregamos a la pila de archivos */
+    public void checkArchivos() throws FileNotFoundException, IOException {
+        File archivo = new File(path + entidadActual.getNombre() + ".dat");
         
-        while(!crearEntidades.empty()) {//este vacio
-            Entidad en = crearEntidades.pop();
-            String nombreArchivo = path + en.getNombre() + ".txt";
+        if (archivo.isFile()) {
+            BufferedReader br = new BufferedReader(new FileReader(archivo));
+            String linea = br.readLine();
             
-            if (DEBUG)
-                System.out.println("Archivo: " + nombreArchivo);
+            archivos.add(linea);
+        }
+    }
+    
+    /* Agregamos un registro en la pila archivos */
+    public boolean addRegistro(String attrs) { // El parametro es un string con los atributos separados por coma.
+        String[] separado = attrs.split(",");
+        
+        /* Si los atributos no sobrepasan el numero de atributos de la entidad lo guardamos */
+        if (separado.length <= entidadActual.getAtributos().size()) {
+            archivos.add(attrs);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    /* Removemos un registro de la pila archivos */
+    public boolean removeRegistro(String attrs) { // El parametro es un string con los atributos separados por coma.
+        String[] separado = attrs.split(",");
+        
+        /* Si los atributos no sobrepasan el numero de atributos de la entidad lo buscamos */
+        if (separado.length <= entidadActual.getAtributos().size()) {
+            Stack<String> borrar = archivos;
+            Stack<String> nuevoArchivo = new Stack<String>(); // Creamos una nueva pila.
             
+            while(!borrar.empty()) {
+                String registro = borrar.pop();
+                
+                if (!registro.equals(attrs)) { // Si el registro es indiferente al que debe ser eliminado lo agregamos a la nueva pila
+                    nuevoArchivo.add(registro);
+                }
+                
+            }
+            
+            archivos = nuevoArchivo; // Los archivos se cambian por la nueva pila que contiene los registros sin el que debia ser eliminado.
+            return true;
+        }
+        
+        return false;
+    }
+    
+    /* Guardamos los datos en los archivos */
+    public void continuar() throws IOException {
+        if (entidadActual != null) { // Si la BD es manager guardos los registros en el archivo .dat
+            Stack<String> aContinuar = archivos;
+            
+            /* Vaciamos el archivo de registros para agregarlos nuevamente */
+            String nombreArchivo = path + entidadActual.getNombre() + ".dat";
             BufferedWriter bf = new BufferedWriter(new FileWriter(nombreArchivo));
-            
-            bf.write(en.getNombre() + "," + en.getAtributosSql());
+            bf.write("");
             bf.close();
+            
+            while(!aContinuar.empty()) {
+                String registro = aContinuar.pop();
+
+                if (DEBUG)
+                    System.out.println("Archivo: " + nombreArchivo);
+
+                
+                bf = new BufferedWriter(new FileWriter(nombreArchivo, true));
+
+                bf.write(registro);
+                bf.newLine();
+                bf.close();
+            }
+            
+        } else { // Si la BD no es manager agregamos las entidades nuevas a la carpeta.
+            Stack<Entidad> mostrarEntidades = entidades;
+        
+            while(!mostrarEntidades.empty()) {
+                Entidad en = mostrarEntidades.pop();
+                String nombreArchivo = path + en.getNombre() + ".txt";
+
+                if (DEBUG)
+                    System.out.println("Archivo: " + nombreArchivo);
+
+                BufferedWriter bf = new BufferedWriter(new FileWriter(nombreArchivo));
+
+                bf.write(en.getNombre() + "," + en.getAtributosSql());
+                bf.close();
+            }
         }
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,15 +3,20 @@ import java.io.IOException;
 
 
 public class Menu {
-	private static String CURRENT_DIR = System.getProperty("user.dir");
-    private static String ENTITIES_DIR = CURRENT_DIR + File.separator + "entidades" + File.separator;
-    
+	
+	 /* Obtenemos la dirección actual del proyecto */
+	private static String ACTUAL_DIR = System.getProperty("user.dir");
+	// Ruta para los archivos de entidades
+	private static String ENTIDAD_DIR = ACTUAL_DIR + File.separator + "entidades" + File.separator;
+	// Ruta para los registros por entidad
+	 private static String REGISTROS_DIR = ACTUAL_DIR + File.separator + "registros" + File.separator;
+	
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        BaseDato db = new BaseDato(ENTITIES_DIR);
-        //db.checkEntidades();
+        BaseDato db = new BaseDato(ENTIDAD_DIR);
+        db.checkEntidades();
         
         Entidad en = new Entidad();
         en.setNombre("alumno");
@@ -22,13 +27,32 @@ public class Menu {
         en.addAtributo("peso", Atributo.TYPE_DOUBLE, 0);
         db.addEntidad(en);
         
-        en = new Entidad();
-        en.setNombre("curso");
-        en.addAtributo("nombre", Atributo.TYPE_STRING, 100);
-        db.addEntidad(en);
+        Entidad curso = new Entidad();
+        curso.setNombre("curso");
+        curso.addAtributo("nombre", Atributo.TYPE_STRING, 100);
+        db.addEntidad(curso);
         
-        db.mostrarEntidad();
-        db.archivoEntidad();
+        db.mostrarEntidades();
+        db.continuar();
+        
+        System.out.println("LOS ATRIBUTOS .DAT SERAN CREADOS Y GUARDADOS EN LA DIRECCION:");
+        BaseDato manager = new BaseDato(REGISTROS_DIR);
+        manager.setToPersist(en);
+        
+        if (manager.addRegistro("Pedro, 15-02-1990, 7690183135, 45568978, 185.2")) {
+            manager.addRegistro("Juan, 15-02-1991, 7690183135, 45568978, 185.2");
+            manager.addRegistro("Solo, 15-02-1989, 7690183135, 45568978, 185.2");
+            manager.removeRegistro("Juan, 15-02-1991, 7690183135, 45568978, 185.2");
+            manager.continuar();
+        } else {
+            System.out.println("Hubo un error al agregar el registro.");
+        }
+        
+     // Para guardador los cambios de la BD siempre que querras agregar una entidad o guardar registros, despues de agregarlos corres el continuar() de la clase BD.
+        /* Para poder guardar registros de una entidad tenes que crear una nueva instancia de BD y convertir esa instancia en un manager de la entidad para hacer eso solo tenes que 
+        setearle un entidad a la BD (con el método setToPersist()) */
+    }
+    
     }
 
-}
+
